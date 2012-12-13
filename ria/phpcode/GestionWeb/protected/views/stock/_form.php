4a6fc938277eee9122832168ef61ba21cb5ef541
nicolasfcastro <nicolasfcastro@gmail.com>
2012-12-13 12:00:19 -0300

    CRUD stock

---
+++ ria/phpcode/GestionWeb/protected/views/stock/_form.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this StockController */
+/* @var $model Stock */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'stock-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'idproducto'); ?>
+		<?php echo $form->textField($model,'idproducto'); ?>
+		<?php echo $form->error($model,'idproducto'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'cantidad'); ?>
+		<?php echo $form->textField($model,'cantidad'); ?>
+		<?php echo $form->error($model,'cantidad'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'cantidadMIN'); ?>
+		<?php echo $form->textField($model,'cantidadMIN'); ?>
+		<?php echo $form->error($model,'cantidadMIN'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'cantidadMAX'); ?>
+		<?php echo $form->textField($model,'cantidadMAX'); ?>
+		<?php echo $form->error($model,'cantidadMAX'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
