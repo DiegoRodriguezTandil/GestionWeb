0f06e4e9645136424a27a60dec3aa873346318b9
nicolasfcastro <nicolasfcastro@gmail.com>
2012-12-13 11:10:39 -0300

    CRUD Categoria producto

---
+++ ria/phpcode/GestionWeb/protected/views/categoriaProducto/view.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this CategoriaProductoController */
+/* @var $model CategoriaProducto */
+
+$this->breadcrumbs=array(
+	'Categoria Productos'=>array('index'),
+	$model->id,
+);
+
+$this->menu=array(
+	array('label'=>'List CategoriaProducto', 'url'=>array('index')),
+	array('label'=>'Create CategoriaProducto', 'url'=>array('create')),
+	array('label'=>'Update CategoriaProducto', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete CategoriaProducto', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage CategoriaProducto', 'url'=>array('admin')),
+);
+?>
+
+<h1>View CategoriaProducto #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'categoria',
+	),
+)); ?>
