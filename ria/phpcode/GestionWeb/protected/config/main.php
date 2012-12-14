bc8eaf9b81e09693ad2f03a2cb4ad520ac3f07fb
DiegoRodriguezTandil <diegorodriguez.tandil@gmail.com>
2012-12-14 00:41:27 -0300

    Agregue clave en la configuracion de la BD y .gitignore en runtime folder

--- ria/phpcode/GestionWeb/protected/config/main.php
+++ ria/phpcode/GestionWeb/protected/config/main.php
@@ -56,7 +56,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=gestion',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'mysqlpass',
 			'charset' => 'utf8',
 		),
 		
