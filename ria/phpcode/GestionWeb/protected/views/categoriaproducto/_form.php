faf104386ab33116c63bd5f7dad5ec1396b4568f
nicolasfcastro <nicolasfcastro@gmail.com>
2012-12-14 16:26:33 -0300

    crud categoria producto

---
+++ ria/phpcode/GestionWeb/protected/views/categoriaproducto/_form.php
@@ -0,0 +1,30 @@
+<?php
+/* @var $this CategoriaproductoController */
+/* @var $model Categoriaproducto */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'categoriaproducto-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'categoria'); ?>
+		<?php echo $form->textField($model,'categoria',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'categoria'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
